1dff565020633ce5e8b4fcdbc704ddacc68316a2
muradtag <murad.tag.97@gmail.com>
2022-03-22 03:52:26 +0200

    Initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  wwoApiKey: 'd3eff03e2fed4ae9813152851222103',
 };
 
 /*
