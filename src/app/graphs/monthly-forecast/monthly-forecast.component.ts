9c353792dd43cb840c89474aaa4282ab71b6abbb
muradtag <murad.tag.97@gmail.com>
2022-03-26 22:35:54 +0200

    minor changes

--- src/app/graphs/monthly-forecast/monthly-forecast.component.ts
+++ src/app/graphs/monthly-forecast/monthly-forecast.component.ts
@@ -13,7 +13,7 @@ export class MonthlyForecastComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    console.log(this.monthlyData);
+    // console.log(this.monthlyData);
 
     let margin = {
       top: 15,
@@ -40,7 +40,7 @@ export class MonthlyForecastComponent implements OnInit {
 
     let graphData = this.monthlyData.data.weather;
 
-    console.log(graphData);
+    // console.log(graphData);
 
     for (let i = 0, temp_counter = 1, len = graphData.length; i < len; i++) {
       // Parse Date and Chance of rain
@@ -69,7 +69,7 @@ export class MonthlyForecastComponent implements OnInit {
       }
     }
 
-    console.log(graphData);
+    // console.log(graphData);
 
     // X and Y scales
     let xScale = d3
@@ -207,268 +207,5 @@ export class MonthlyForecastComponent implements OnInit {
         if (d.uvIndex != 0) return d.uvIndex + '';
         else return '';
       });
-
-    // Get the data
-    // d3.json(
-    //   'https://gist.githubusercontent.com/bojanvidanovic/4bfcdf766867bd65c6b07882c01abd9f/raw/8ce8482f9e2335d15c5b1a3bdd7573c3cc105318/forecast.json'
-    // ).then((data: any) => {
-    //   // console.log('API data', data);
-    //   // Hourly forecast data
-    //   var data = data.hourly_forecast;
-
-    //   // Format the data
-    //   for (
-    //     var i = 0, temp_counter = 1, icon_counter = 1, len = data.length;
-    //     i < len;
-    //     i++
-    //   ) {
-    //     // Parse time
-    //     var timeBase = data[i].FCTTIME;
-    //     var hour = timeBase.hour_padded;
-    //     var minutes = timeBase.min;
-    //     var day = timeBase.mday_padded;
-    //     var month = timeBase.mon_padded;
-    //     var year = timeBase.year;
-    //     var date = [hour, minutes, day, month, year].join(' ');
-    //     data[i].time = parseDate(date);
-
-    //     // POP value to Int
-    //     data[i].pop = +data[i].pop;
-
-    //     // Group temperature
-    //     if (i < len - 1) {
-    //       if (data[i].temp.metric === data[i + 1].temp.metric) {
-    //         temp_counter++;
-    //       } else {
-    //         var moveHere = i - Math.round(--temp_counter / 2);
-    //         data[moveHere].temp_read = data[i].temp.metric;
-    //         temp_counter = 1;
-    //       }
-    //     } else {
-    //       if (data[i].temp.metric === data[i - 1].temp.metric) {
-    //         var moveHere = i - Math.round(--temp_counter / 2);
-    //         data[moveHere].temp_read = data[i].temp.metric;
-    //         temp_counter = 1;
-    //       } else {
-    //         data[i].temp_read = data[i].temp.metric;
-    //       }
-    //     }
-
-    //     // Group weather icons
-    //     if (i < len - 1) {
-    //       if (data[i].icon_url === data[i + 1].icon_url) {
-    //         if (data[i + 2]) {
-    //           icon_counter++;
-    //         }
-    //       } else {
-    //         if (icon_counter === 1) {
-    //           data[i].graph_icon = data[i].icon_url;
-    //           data[i + 1].icon_limit = true;
-    //         } else {
-    //           var moveHere = i - Math.round(icon_counter / 2);
-    //           data[moveHere].graph_icon = data[i].icon_url;
-    //           data[i].icon_limit = true;
-    //           icon_counter = 1;
-    //         }
-    //       }
-    //     } else {
-    //       if (data[i].icon_url === data[i - 1].icon_url) {
-    //         var moveHere = i - Math.round(icon_counter / 2) - 1;
-    //         data[moveHere].graph_icon = data[i].icon_url;
-    //       } else {
-    //         data[i].graph_icon = data[i].icon_url;
-    //         icon_counter = 1;
-    //       }
-    //     }
-    //   }
-
-    //   // console.log("API Result", data);
-
-    //   // let x = data.map((d: any) => d.temp.metric);
-    //   // console.log(x);
-    //   // console.log(d3.max(data, (d: any) => Number(d.temp.metric)));
-
-    //   // Set scales
-    //   var xScale = d3
-    //     .scaleTime()
-    //     .domain(d3.extent(data, (d: any) => d.time) as Iterable<Date>)
-    //     .range([0, width]);
-
-    //   var yScale = d3
-    //     .scaleLinear()
-    //     .domain([
-    //       Number(d3.min(data, (d: any) => Number(d.temp.metric))),
-    //       Number(d3.max(data, (d: any) => Number(d.temp.metric))),
-    //     ])
-    //     .range([60, height - 30]);
-
-    //   // console.log(xScale);
-
-    //   // Bottom scale
-    //   let x_axis = d3
-    //     .axisBottom(xScale)
-    //     .tickFormat((d) => timeFormat(d as Date));
-    //   svg
-    //     .append('g')
-    //     .attr('transform', 'translate(0,' + height + ')')
-    //     .call(x_axis);
-
-    //   // Draw temperature line
-    //   svg
-    //     .append('path')
-    //     .datum(data)
-    //     .style('stroke', 'steelblue')
-    //     .style('stroke-width', '3')
-    //     .style('fill', 'none')
-    //     .attr(
-    //       'd',
-    //       d3
-    //         .line()
-    //         .curve(d3.curveCardinal)
-    //         .x((d: any) => xScale(d.time))
-    //         .y((d: any) => height - yScale(d.temp.metric))
-    //     );
-
-    //   // Fill temperature area
-    //   svg
-    //     .append('path')
-    //     .datum(data)
-    //     .attr('fill', 'steelblue')
-    //     .attr('stroke-width', '0')
-    //     .attr('opacity', '.2')
-    //     .attr(
-    //       'd',
-    //       d3
-    //         .area()
-    //         .curve(d3.curveCardinal)
-    //         .x((d: any) => xScale(d.time))
-    //         .y0(height)
-    //         .y1((d: any) => height - yScale(d.temp.metric))
-    //     );
-
-    //   // console.log(
-    //   //   data.filter((d: any) => {
-    //   //     return d.temp_read;
-    //   //   })
-    //   // );
-
-    //   // Temperature change point and value
-    //   var tempPoint = svg
-    //     .selectAll('.temp-point')
-    //     .data(data)
-    //     .enter()
-    //     .filter((d: any) => {
-    //       return d.temp_read;
-    //     });
-
-    //   tempPoint
-    //     .append('circle')
-    //     .attr('fill', 'steelblue')
-    //     .attr('stroke', 'steelblue')
-    //     .attr('stroke-width', '2')
-    //     .attr('cx', (d: any) => {
-    //       return xScale(d.time);
-    //     })
-    //     .attr('cy', (d: any) => {
-    //       return height - yScale(d.temp_read);
-    //     })
-    //     .attr('r', 3);
-
-    //   tempPoint
-    //     .append('text')
-    //     .attr('x', (d: any) => xScale(d.time))
-    //     .attr('y', (d: any) => height - yScale(d.temp_read))
-    //     .attr('dy', '-10px')
-    //     .style('text-anchor', 'middle')
-    //     .text((d: any) => d.temp_read + '°');
-
-    //   // Define POP Y scale
-    //   var popY = d3
-    //     .scaleLinear()
-    //     .domain([0, Number(d3.max(data, (d: any) => d.pop))])
-    //     .range([0, 20]);
-
-    //   // Draw POP line
-    //   svg
-    //     .append('path')
-    //     .datum(data)
-    //     .attr('stroke', '#000')
-    //     .attr('stroke-width', '.3')
-    //     .attr('fill', 'none')
-    //     .attr(
-    //       'd',
-    //       d3
-    //         .line()
-    //         .curve(d3.curveStep)
-    //         .x((d: any) => xScale(d.time))
-    //         .y((d: any) => height - popY(d.pop))
-    //     );
-
-    //   // Paint POP Area
-    //   svg
-    //     .append('path')
-    //     .datum(data)
-    //     .attr('class', 'pop-area')
-    //     .attr('fill', '#3590df')
-    //     .attr('stroke-width', '0')
-    //     .attr(
-    //       'd',
-    //       d3
-    //         .area()
-    //         .curve(d3.curveStep)
-    //         .x((d: any) => xScale(d.time))
-    //         .y0(height)
-    //         .y1((d: any) => height - popY(d.pop))
-    //     );
-
-    //   // POP value
-    //   var pop = svg
-    //     .selectAll('.pop-value')
-    //     .data(data)
-    //     .enter()
-    //     .append('text')
-    //     .attr('class', 'pop-value')
-    //     .attr('font-size', '8px')
-    //     .attr('x', (d: any) => xScale(d.time))
-    //     .attr('y', (d: any) => height - popY(d.pop))
-    //     .attr('dy', '-5px')
-    //     .style('text-anchor', 'middle')
-    //     .text(function (d: any) {
-    //       if (d.pop != 0) return d.pop + '%';
-    //       else return '';
-    //     });
-
-    //   // Forecast icon
-    //   svg
-    //     .selectAll('image')
-    //     .data(data)
-    //     .enter()
-    //     .filter((d: any) => {
-    //       if (d.graph_icon) return d.time;
-    //     })
-    //     .append('image')
-    //     .attr('xlink:href', (d: any) => d.graph_icon)
-    //     .attr('height', '19')
-    //     .attr('width', '20')
-    //     .attr('x', (d: any) => xScale(d.time))
-    //     .attr('y', 65);
-
-    //   // Separate forecast icons
-    //   svg
-    //     .selectAll('.divider-line')
-    //     .data(data)
-    //     .enter()
-    //     .filter((d: any) => {
-    //       if (d.icon_limit) return d.time;
-    //     })
-    //     .append('line')
-    //     .style('stroke-dasharray', '3,3')
-    //     .style('stroke', 'grey')
-    //     .attr('class', 'divider-line')
-    //     .attr('x1', (d: any) => xScale(d.time))
-    //     .attr('y1', height)
-    //     .attr('x2', (d: any) => xScale(d.time))
-    //     .attr('y2', (d: any) => height - yScale(d.temp.metric) + 2);
-    // });
   }
 }
